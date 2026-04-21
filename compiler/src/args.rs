96803db0dbe333be9cd6f83706a266add2a55b66
Karan Shah <karanshah0206@outlook.com>
2026-04-21 04:42:30 -0400

    Comments in args.rs

--- compiler/src/args.rs
+++ compiler/src/args.rs
@@ -1,26 +1,39 @@
 use std::env;
 
-/// Target code
+/// Target (output) language for the compiler.
 pub enum EmitTarget {
+  /// SSA-based 3-operand abstract assembly (IR)
   Abstract,
+  /// SysV AMD64 GNU/AT&T assembly
   X86_64,
+  /// LLVM IR
   Llvm,
 }
 
-/// Configuration options for the compiler
+/// Configuration options for the compiler.
 pub struct Config {
+  /// Print time profiling by stages to terminal.
   pub verbose: bool,
+  /// Print abstract syntax tree to terminal.
   pub dump_ast: bool,
+  /// Stop after lexical, syntactic, and semantic analysis.
   pub check: bool,
+  /// Desired level of optimizations.
   pub optimizer_level: u8,
+  /// Enable or disable memory and arithmetic safety checks.
   pub allow_unsafe: bool,
+  /// Print SSA intermediate representation to terminal.
   pub dump_ir: bool,
+  /// Target language for the compiler.
   pub target: EmitTarget,
+  /// Path to file of external declarations to link with source code.
   pub header: Option<String>,
+  /// Path to source code file.
   pub source: Option<String>,
 }
 
 impl Config {
+  /// Generate a default config options template.
   fn defalut() -> Self {
     Config {
       verbose: false,
@@ -36,6 +49,7 @@ impl Config {
   }
 }
 
+/// Produce a config from arguments passed by command line.
 pub fn parse_args() -> Config {
   let args: Vec<String> = env::args().collect();
   let mut config = Config::defalut();
